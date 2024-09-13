25dc134f9cc7615f2de4bc0431a24dd31862dfcb
OniWithTheHoodie <144009897+OniWithTheHoodie@users.noreply.github.com>
2024-09-13 14:43:27 +0200

    notes
    i love web notes

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script>
+    // Import global styles 
+    import '../global.css';
+  </script>
+
+<header>
+  <!-- header content here -->
+</header>
+
+<main>
+  <slot /> <!-- This renders content from +page.svelte -->
+</main>
+
+<footer>
+  <!-- footer content here -->
+</footer>
+
+<style>
+  /* specific styles here */
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,32 @@
+<main>
+    <h1>Digital gardning</h1>
+    <h2></h2>
+    <h3>2-9-2024</h3>
+    <ol>
+        <li>Hoe kan ik een goeie leer strategie te kunnen maken om Sveltekit te kunnen beheersen ?</li>
+        <li>Hoe kan ik mijn bewijstlast op de juiste manier opleveren ?</li>
+        <li>Hoe kan ik een logische stappenplan maken om complexe interacties te kunnen bouwen ?</li>
+    </ol>
+    <h3>4-9-2024</h3>
+    <p>
+        Svelte project begonnen voor de your tribe squad page.ik had zelf in de vakantie al een beetje naar Svelte uitgeprobeerd en geïnstalleerd.Dus voor de your tribe squad page zou ik alleen een moeten forken en een npm install te doen alleen kreeg ik hier wat problemen mee via de terminal. Ik kreeg vaak een fout melding.
+        Dus heb ik hiervoor een dummy bestand gemaakt en de bestanden in your tribe squad page in de dummy bestand verplaatst en verwijderd.
+        vervolgens heb ik zelf een nieuwe bestand gemaakt genaamd your tribe squad page zodat ik het kon pushen in de zelfde repo dan hoef ik geen hele nieuwe aan te maken.
+        toen ik nieuwe bestand had aangemaakt was de installatie als volgt.
+    </p>
+    <ol>
+        <li>npm create svelte@latest</li>
+        <li>cd bestand locatie</li>
+        <li>npm install</li>
+        <li>npm run dev -- -- open</li>
+        <li>om de web pagina te kunnen openen</li>
+        <li>vervolgens kon ik dit ook pushen naar de zelfde repo zonder wat kwijt te raken wat al was aangemaakt zoals mij projectboard en aangemaakte issues.</li>
+    </ol>
+</main>
+
+<style>
+    h1 {
+        font-family: 'Rubik Moonrocks';
+        color: var(--text-color);
+    }
+</style>
