57159d318302782577bfbfd9561c3b9322e86963
OniWithTheHoodie <144009897+OniWithTheHoodie@users.noreply.github.com>
2024-09-20 04:53:15 +0200

    eerste push
    eerste push i-love-web

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,9 +3,20 @@
     <h2></h2>
     <h3>2-9-2024</h3>
     <ol>
-        <li>Hoe kan ik een goeie leer strategie te kunnen maken om Sveltekit te kunnen beheersen ?</li>
+        <li>Hoe kan ik een goeie leer strategie maken om Sveltekit te kunnen beheersen ?</li>
+        <p>Ik gebruik hier voor de tutorial van Svelte waarbij ik de mogelijkheden binnen svelte uitgeprobeer
+            daarbij probeer ik het ook uit in side projects of de projecten van school.
+        </p>
         <li>Hoe kan ik mijn bewijstlast op de juiste manier opleveren ?</li>
+        <p>Het allereerste is in de repo de docs doornemen zodat ik weet wat er verwacht word van de opdracht die ik moet maken.
+            daarbij de DOD volgen en dat afhandelen. Verwerken in de digital gardning.
+        </p>
         <li>Hoe kan ik een logische stappenplan maken om complexe interacties te kunnen bouwen ?</li>
+        <p>Ik probeer en test javascript functies om te kijken wat de mogelijkheden zijn en of ik dat kan 
+            toepassen in mijn eigen project het zelfde geld voor css waarbij Vasilis zei gewoon iets uitproberen.
+            ook al is het gek waarbij ik probeer te spelen met css transitions, scale etc. Maar het idee is om te kijken.
+            het in mijn eigen project toe te passen dat is iets waar ik nog naar aan het kijken.
+        </p>
     </ol>
     <h3>4-9-2024</h3>
     <p>
@@ -22,6 +33,31 @@
         <li>om de web pagina te kunnen openen</li>
         <li>vervolgens kon ik dit ook pushen naar de zelfde repo zonder wat kwijt te raken wat al was aangemaakt zoals mij projectboard en aangemaakte issues.</li>
     </ol>
+
+    <h2>We love web: Vasilis van Gemert</h2>
+    <p>Vasilis houd van het web en is  zelf een kunstenaar Hij maakte dingen met bijvoorbeeld glas of fotografie.</p>
+    <p>Vervolgens had hij een computer gekocht en kwam hij erachter dat je leuke dingen kon doen op het web. Eenigste wat hij nodig was is een computer en een editor dat gratis is</p>
+    <p>In zijn tijd bestond flexbox nog niet. Toen het uit kwam ging hij er mee spelen om te kijken wat de mogelijkheden waren. Waardoor hij een leuke en chaotische design had kunnen maken.</p>
+    <p>Hij zegt ook dat het handig is om onzinnige dingen te maken. Door dit te doen kun je coole nieuwe dingen maken en je leert er ook van.</p>
+    <p>Hij vondt het ook leuk om klokken te maken. Hij deed onderzoek er naar en er kwamen meer css technieken uit en besloot hij verschillende klokken designs te maken. Hij maakte een digitale klok waarbij er mini klokken om heen waren 
+        die draaide terwijl de tijd van de digitale klok veranderde. Ook maakte hij een klok van blokken met verschilende kleuren dat telde telde in binnary.
+    </p>
+    <p>Hij begon ook aan een project dat het heel veel tijd koste. Dit project besloot hij een klok te maken dat foto's toont dat precies op de aangegeven tijd staat. Hij vondt toegangekelijkheid ook erg belangrijk. 
+        Hij vroeg ook aan ons hoeveel minuten er in een klok zitten waarbij het antwoord was 1440. Hij moest hiervoor elke foto een omschrijving meegeven wat super veel werk is. Hij zei ook dat hij in sommige gevalen javascript nodig had
+        waar hij zelf niet goed in is maar hij houd er van om uren te nerden op nieuwe technieken maar css is waar hij gelukkig van wordt.
+    </p>
+    <p>Hij verteld ook dat wat toegangekelijkheid betreft het belangerijk is dat je goed checkt of de website ook werkelijk toegangekelijk is en dat niet dingen worden afgevinkt en wordt gezegt ja dit is goed genoeg. 
+        Er is een site van NPO met allerlei documenteres. Waarbij een jongen op een late leeftijd blind is geworden. Blinde mensen gebruiken screen readers en hij was er niet blij mee toen hij gebruik maakte van de website.
+        Er was zoveel content op de site en alles word semantisch opgelezen. Zoals heading 1, heading 2 etc. dat wordt voorgelezen door de screen reader en dat is niet fijn voor iemand met een beperking. Hij was verbaasd
+        naar de reactie en besloot hij de web pagina te redesignen.
+    </p>
+    <p>Hij had nog een site gemaakt voor iemand die blind was. waardoor de gebruiker door de pagina heen ging navigeren met de tab. En het duurde zolang voor hij bij de pagina kwam die hij nodig had. Hij moest door door velen
+        navigaties heen waarbij linkjes in zaten die worden voorgelezen. En dat koste voor hem 2min wat voor gebruikers die geen beperking hebben 2 seconde kost.
+    </p>
+    <p>Ik vroeg aan hem na de presentatie hoe hij dit had opgelost. Hij deed dit door all navigatie weg te halen en te focusen op de content. Hij vertelde daarbij dat soms de oplossing zo simple is. want velen
+        blinde mensen waar hij mee gewerkt heeft vragen zich af waarom er zoveel navigatie links die onnodig veel dingen lezen. Terwijl zij simpel gebruik willen maken van de content die zij nodig hebben.
+    </p>
+    <p>Vasilis vind dat de zieke dingen die hij heeft gemaakt wij dat ook kunnen zolang je het maar wilt en bereid bent nieuwe of oude technieken mee gaat experimenten en er zieken dingen mee maakt.</p>
 </main>
 
 <style>
@@ -30,3 +66,4 @@
         color: var(--text-color);
     }
 </style>
+
