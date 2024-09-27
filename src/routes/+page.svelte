199fe3e89b13a121480b67e9fb05c7e0dc606bac
OniWithTheHoodie <144009897+OniWithTheHoodie@users.noreply.github.com>
2024-09-28 01:23:13 +0200

    /
    .

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,99 +1,123 @@
 <main>
     <h1>Digital gardning</h1>
-    <h2></h2>
-    <h3>2-9-2024</h3>
-    <ol>
-        <li>Hoe kan ik een goeie leer strategie maken om Sveltekit te kunnen beheersen ?</li>
-        <p>Ik gebruik hier voor de tutorial van Svelte waarbij ik de mogelijkheden binnen svelte uitgeprobeer
-            daarbij probeer ik het ook uit in side projects of de projecten van school.
+    <article>
+        <h3>2-9-2024</h3>
+        <ol>
+            <li>Hoe kan ik een goeie leer strategie maken om Sveltekit te kunnen beheersen ?</li>
+            <p>Ik gebruik hier voor de tutorial van Svelte waarbij ik de mogelijkheden binnen svelte uitgeprobeer
+                daarbij probeer ik het ook uit in side projects of de projecten van school.
+            </p>
+            <li>Hoe kan ik mijn bewijstlast op de juiste manier opleveren ?</li>
+            <p>Het allereerste is in de repo de docs doornemen zodat ik weet wat er verwacht word van de opdracht die ik moet maken.
+                daarbij de DOD volgen en dat afhandelen. Verwerken in de digital gardning.
+            </p>
+            <li>Hoe kan ik een logische stappenplan maken om complexe interacties te kunnen bouwen ?</li>
+            <p>Ik probeer en test javascript functies om te kijken wat de mogelijkheden zijn en of ik dat kan 
+                toepassen in mijn eigen project het zelfde geld voor css waarbij Vasilis zei gewoon iets uitproberen.
+                ook al is het gek waarbij ik probeer te spelen met css transitions, scale etc. Maar het idee is om te kijken.
+                het in mijn eigen project toe te passen dat is iets waar ik nog naar aan het kijken.
+            </p>
+        </ol>
+    </article>
+    <article>   
+        <h3>4-9-2024</h3>
+        <p>
+            Svelte project begonnen voor de your tribe squad page.ik had zelf in de vakantie al een beetje naar Svelte uitgeprobeerd en geïnstalleerd.Dus voor de your tribe squad page zou ik alleen een moeten forken en een npm install te doen alleen kreeg ik hier wat problemen mee via de terminal. Ik kreeg vaak een fout melding.
+            Dus heb ik hiervoor een dummy bestand gemaakt en de bestanden in your tribe squad page in de dummy bestand verplaatst en verwijderd.
+            vervolgens heb ik zelf een nieuwe bestand gemaakt genaamd your tribe squad page zodat ik het kon pushen in de zelfde repo dan hoef ik geen hele nieuwe aan te maken.
+            toen ik nieuwe bestand had aangemaakt was de installatie als volgt.
         </p>
-        <li>Hoe kan ik mijn bewijstlast op de juiste manier opleveren ?</li>
-        <p>Het allereerste is in de repo de docs doornemen zodat ik weet wat er verwacht word van de opdracht die ik moet maken.
-            daarbij de DOD volgen en dat afhandelen. Verwerken in de digital gardning.
+        <ol>
+            <li>npm create svelte@latest</li>
+            <li>cd bestand locatie</li>
+            <li>npm install</li>
+            <li>npm run dev -- -- open</li>
+            <li>om de web pagina te kunnen openen</li>
+            <li>vervolgens kon ik dit ook pushen naar de zelfde repo zonder wat kwijt te raken wat al was aangemaakt zoals mij projectboard en aangemaakte issues.</li>
+        </ol>
+    </article>
+
+    <article>
+        <h2>We love web: Vasilis van Gemert</h2>
+        <p>Vasilis houd van het web en is  zelf een kunstenaar Hij maakte dingen met bijvoorbeeld glas of fotografie.</p>
+        <p>Vervolgens had hij een computer gekocht en kwam hij erachter dat je leuke dingen kon doen op het web. Eenigste wat hij nodig was is een computer en een editor dat gratis is</p>
+        <p>In zijn tijd bestond flexbox nog niet. Toen het uit kwam ging hij er mee spelen om te kijken wat de mogelijkheden waren. Waardoor hij een leuke en chaotische design had kunnen maken.</p>
+        <p>Hij zegt ook dat het handig is om onzinnige dingen te maken. Door dit te doen kun je coole nieuwe dingen maken en je leert er ook van.</p>
+        <p>Hij vondt het ook leuk om klokken te maken. Hij deed onderzoek er naar en er kwamen meer css technieken uit en besloot hij verschillende klokken designs te maken. Hij maakte een digitale klok waarbij er mini klokken om heen waren 
+            die draaide terwijl de tijd van de digitale klok veranderde. Ook maakte hij een klok van blokken met verschilende kleuren dat telde telde in binnary.
         </p>
-        <li>Hoe kan ik een logische stappenplan maken om complexe interacties te kunnen bouwen ?</li>
-        <p>Ik probeer en test javascript functies om te kijken wat de mogelijkheden zijn en of ik dat kan 
-            toepassen in mijn eigen project het zelfde geld voor css waarbij Vasilis zei gewoon iets uitproberen.
-            ook al is het gek waarbij ik probeer te spelen met css transitions, scale etc. Maar het idee is om te kijken.
-            het in mijn eigen project toe te passen dat is iets waar ik nog naar aan het kijken.
+        <p>Hij begon ook aan een project dat het heel veel tijd koste. Dit project besloot hij een klok te maken dat foto's toont dat precies op de aangegeven tijd staat. Hij vondt toegangekelijkheid ook erg belangrijk. 
+            Hij vroeg ook aan ons hoeveel minuten er in een klok zitten waarbij het antwoord was 1440. Hij moest hiervoor elke foto een omschrijving meegeven wat super veel werk is. Hij zei ook dat hij in sommige gevalen javascript nodig had
+            waar hij zelf niet goed in is maar hij houd er van om uren te nerden op nieuwe technieken maar css is waar hij gelukkig van wordt.
         </p>
-    </ol>
-    <h3>4-9-2024</h3>
-    <p>
-        Svelte project begonnen voor de your tribe squad page.ik had zelf in de vakantie al een beetje naar Svelte uitgeprobeerd en geïnstalleerd.Dus voor de your tribe squad page zou ik alleen een moeten forken en een npm install te doen alleen kreeg ik hier wat problemen mee via de terminal. Ik kreeg vaak een fout melding.
-        Dus heb ik hiervoor een dummy bestand gemaakt en de bestanden in your tribe squad page in de dummy bestand verplaatst en verwijderd.
-        vervolgens heb ik zelf een nieuwe bestand gemaakt genaamd your tribe squad page zodat ik het kon pushen in de zelfde repo dan hoef ik geen hele nieuwe aan te maken.
-        toen ik nieuwe bestand had aangemaakt was de installatie als volgt.
-    </p>
-    <ol>
-        <li>npm create svelte@latest</li>
-        <li>cd bestand locatie</li>
-        <li>npm install</li>
-        <li>npm run dev -- -- open</li>
-        <li>om de web pagina te kunnen openen</li>
-        <li>vervolgens kon ik dit ook pushen naar de zelfde repo zonder wat kwijt te raken wat al was aangemaakt zoals mij projectboard en aangemaakte issues.</li>
-    </ol>
+        <p>Hij verteld ook dat wat toegangekelijkheid betreft het belangerijk is dat je goed checkt of de website ook werkelijk toegangekelijk is en dat niet dingen worden afgevinkt en wordt gezegt ja dit is goed genoeg. 
+            Er is een site van NPO met allerlei documenteres. Waarbij een jongen op een late leeftijd blind is geworden. Blinde mensen gebruiken screen readers en hij was er niet blij mee toen hij gebruik maakte van de website.
+            Er was zoveel content op de site en alles word semantisch opgelezen. Zoals heading 1, heading 2 etc. dat wordt voorgelezen door de screen reader en dat is niet fijn voor iemand met een beperking. Hij was verbaasd
+            naar de reactie en besloot hij de web pagina te redesignen.
+        </p>
+        <p>Hij had nog een site gemaakt voor iemand die blind was. waardoor de gebruiker door de pagina heen ging navigeren met de tab. En het duurde zolang voor hij bij de pagina kwam die hij nodig had. Hij moest door door velen
+            navigaties heen waarbij linkjes in zaten die worden voorgelezen. En dat koste voor hem 2min wat voor gebruikers die geen beperking hebben 2 seconde kost.
+        </p>
+        <p>Ik vroeg aan hem na de presentatie hoe hij dit had opgelost. Hij deed dit door all navigatie weg te halen en te focusen op de content. Hij vertelde daarbij dat soms de oplossing zo simple is. want velen
+            blinde mensen waar hij mee gewerkt heeft vragen zich af waarom er zoveel navigatie links die onnodig veel dingen lezen. Terwijl zij simpel gebruik willen maken van de content die zij nodig hebben.
+        </p>
+        <p>Vasilis vind dat de zieke dingen die hij heeft gemaakt wij dat ook kunnen zolang je het maar wilt en bereid bent nieuwe of oude technieken mee gaat experimenten en er zieken dingen mee maakt.</p>
+    </article>
 
-    <h2>We love web: Vasilis van Gemert</h2>
-    <p>Vasilis houd van het web en is  zelf een kunstenaar Hij maakte dingen met bijvoorbeeld glas of fotografie.</p>
-    <p>Vervolgens had hij een computer gekocht en kwam hij erachter dat je leuke dingen kon doen op het web. Eenigste wat hij nodig was is een computer en een editor dat gratis is</p>
-    <p>In zijn tijd bestond flexbox nog niet. Toen het uit kwam ging hij er mee spelen om te kijken wat de mogelijkheden waren. Waardoor hij een leuke en chaotische design had kunnen maken.</p>
-    <p>Hij zegt ook dat het handig is om onzinnige dingen te maken. Door dit te doen kun je coole nieuwe dingen maken en je leert er ook van.</p>
-    <p>Hij vondt het ook leuk om klokken te maken. Hij deed onderzoek er naar en er kwamen meer css technieken uit en besloot hij verschillende klokken designs te maken. Hij maakte een digitale klok waarbij er mini klokken om heen waren 
-        die draaide terwijl de tijd van de digitale klok veranderde. Ook maakte hij een klok van blokken met verschilende kleuren dat telde telde in binnary.
-    </p>
-    <p>Hij begon ook aan een project dat het heel veel tijd koste. Dit project besloot hij een klok te maken dat foto's toont dat precies op de aangegeven tijd staat. Hij vondt toegangekelijkheid ook erg belangrijk. 
-        Hij vroeg ook aan ons hoeveel minuten er in een klok zitten waarbij het antwoord was 1440. Hij moest hiervoor elke foto een omschrijving meegeven wat super veel werk is. Hij zei ook dat hij in sommige gevalen javascript nodig had
-        waar hij zelf niet goed in is maar hij houd er van om uren te nerden op nieuwe technieken maar css is waar hij gelukkig van wordt.
-    </p>
-    <p>Hij verteld ook dat wat toegangekelijkheid betreft het belangerijk is dat je goed checkt of de website ook werkelijk toegangekelijk is en dat niet dingen worden afgevinkt en wordt gezegt ja dit is goed genoeg. 
-        Er is een site van NPO met allerlei documenteres. Waarbij een jongen op een late leeftijd blind is geworden. Blinde mensen gebruiken screen readers en hij was er niet blij mee toen hij gebruik maakte van de website.
-        Er was zoveel content op de site en alles word semantisch opgelezen. Zoals heading 1, heading 2 etc. dat wordt voorgelezen door de screen reader en dat is niet fijn voor iemand met een beperking. Hij was verbaasd
-        naar de reactie en besloot hij de web pagina te redesignen.
-    </p>
-    <p>Hij had nog een site gemaakt voor iemand die blind was. waardoor de gebruiker door de pagina heen ging navigeren met de tab. En het duurde zolang voor hij bij de pagina kwam die hij nodig had. Hij moest door door velen
-        navigaties heen waarbij linkjes in zaten die worden voorgelezen. En dat koste voor hem 2min wat voor gebruikers die geen beperking hebben 2 seconde kost.
-    </p>
-    <p>Ik vroeg aan hem na de presentatie hoe hij dit had opgelost. Hij deed dit door all navigatie weg te halen en te focusen op de content. Hij vertelde daarbij dat soms de oplossing zo simple is. want velen
-        blinde mensen waar hij mee gewerkt heeft vragen zich af waarom er zoveel navigatie links die onnodig veel dingen lezen. Terwijl zij simpel gebruik willen maken van de content die zij nodig hebben.
-    </p>
-    <p>Vasilis vind dat de zieke dingen die hij heeft gemaakt wij dat ook kunnen zolang je het maar wilt en bereid bent nieuwe of oude technieken mee gaat experimenten en er zieken dingen mee maakt.</p>
+    <article>
+        <h1>19-9-2024(Squad Page)</h1>
+        <p>Voor dit project heb ik samen met Eva, Rukiya, Akiko en Zainab de squadpage gemaakt.
+            Voor de squad page hebben wij een design gemaakt en ook inspiraties die wij hebben opgehaald. van bronnen zoals pintrest, dribble en aaawards. Al deze ideeën hebben
+            hebben vervolgens samen gezet en ontworpen in Figma. 
+        </p>
+        <figure>
+            <img src="images/squadpage/figmaDesign.png" alt="">
+            <img src="images/squadpage/figmaDesign2.png" alt="">
+            <img src="images/squadpage/figmaInspiratie.png" alt="">
+        </figure>
+            <a href="https://www.figma.com/design/R4hylHYnKXGfit6t7KFIS8/your-tribe-for-life-squad-page?node-id=0-1&node-type=canvas&t=3jQMnHQBfk89bHcY-0">link figma</a>
 
-    <h1>19-9-2024(Squad Page)</h1>
-    <p>Voor dit project heb ik samen met Eva, Rukiya, Akiko en Zainab de squadpage gemaakt.
-        Voor de squad page hebben wij een design gemaakt en ook inspiraties die wij hebben opgehaald. van bronnen zoals pintrest, dribble en aaawards. Al deze ideeën hebben
-        hebben vervolgens samen gezet en ontworpen in Figma. 
-    </p>
-    <img src="images/squadpage/figmaDesign.png" alt="">
-    <img src="images/squadpage/figmaDesign2.png" alt="">
-    <img src="images/squadpage/figmaInspiratie.png" alt="">
-    <p>vervolgens hebben wij een taakverdeling gedaan via github waar voor de samenwerking we tijdens school bij elkaar zaten
-        en als we thuis waren hielden wij contact via teams. We hebben daarbij de MOSCOW methode toegepast en gekeken naar wat belangerijk is tot minder belangerijk.
-        Waar wij dingen zoals de team canvas, ontwerp, kampvuur sessie, code conventies etc. in de projectboard hebben geplaatst. Ook voor specifieke taken kreeg of 1 een assignee of meerdere leden.
-        issues die werden ingeschoten werden ook meegenomen in de project board en zoals alle taken als iets af was werd verschoven van to-do naar in progress, in review en done.
-    </p>
-    <img src="images/squadpage/projectboard.png" alt="">
-    <p>En om ervoor te zorgen dat we geen conflicten hadden als het ging om code. deed ik alle pull requesten
-        en controleerde ik voor conflicten en wij hadden als afspraak dat we aparte branches aanmaakte die ik vervolgens met de main merge.
-    </p>
-    <img src="images/squadpage/pullrequest1.png" alt="">
-    <img src="images/squadpage/pullrequest2.png" alt="">
-    <img src="images/squadpage/branches.png" alt="">
-    <p>
-    Het eind resultaat van de squadpage 
-    </p>
-    <video autoplay loop muted>
-        <source src="videos/squadpage/squadpage.mp4" type="video/mp4">
-        Your browser does not support the video tag.
-    </video>
+        <p>vervolgens hebben wij een taakverdeling gedaan via github waar voor de samenwerking we tijdens school bij elkaar zaten
+            en als we thuis waren hielden wij contact via teams. We hebben daarbij de MOSCOW methode toegepast en gekeken naar wat belangerijk is tot minder belangerijk.
+            Waar wij dingen zoals de team canvas, ontwerp, kampvuur sessie, code conventies etc. in de projectboard hebben geplaatst. Ook voor specifieke taken kreeg of 1 een assignee of meerdere leden.
+            issues die werden ingeschoten werden ook meegenomen in de project board en zoals alle taken als iets af was werd verschoven van to-do naar in progress, in review en done.
+        </p>
+        <figure>
+            <img src="images/squadpage/projectboard.png" alt="">
+        </figure>
+        <a href="https://github.com/users/OniWithTheHoodie/projects/12/views/1">link projectboard</a>
+    </article>
 
-    <section>
-        <h1>19-9-2024(Profile card)</h1>
-        <p>Voor de profile card heb ik </p>
+    <article>
+        <p>En om ervoor te zorgen dat we geen conflicten hadden als het ging om code. deed ik alle pull requesten
+            en controleerde ik voor conflicten en wij hadden als afspraak dat we aparte branches aanmaakte die ik vervolgens met de main merge.
+        </p>
+        <a href="https://github.com/users/OniWithTheHoodie/projects/12">Link projectboard</a>
+        <figure>
+            <img src="images/squadpage/pullrequest1.png" alt="">
+            <img src="images/squadpage/pullrequest2.png" alt="">
+        </figure>
+        <a href="https://github.com/OniWithTheHoodie/your-tribe-for-life-squad-page/pulls">link pull request</a>
+        <figure>
+            <img src="images/squadpage/branches.png" alt="">
+        </figure>
+        <p>
+        Het eind resultaat van de squadpage 
+        </p>
         <video autoplay loop muted>
-            <source src="videos/profilecard/profilecard.mp4" type="video/mp4">
+            <source src="videos/squadpage/squadpage.mp4" type="video/mp4">
             Your browser does not support the video tag.
         </video>
+        <a href="https://your-tribe-for-life-squad-page-gray.vercel.app/">link webpagina</a>
+    </article>    
+
+    <section>
+            <h1>19-9-2024(Profile card)</h1>
+            <p>Voor de profile card heb ik </p>
+            <video autoplay loop muted>
+                <source src="videos/profilecard/profilecard.mp4" type="video/mp4">
+                Your browser does not support the video tag.
+            </video>
     </section>
 </main>
 
@@ -111,6 +135,12 @@
     img {
         width: 600px;
         height: 600px;
+    }
+
+    figure {
+        display: flex;
+        flex-direction: row;
+        margin: 10px 0;
     }
 </style>
 
