b51c8cb119de3fe73c843ae9f8e643ba54ff6e26
OniWithTheHoodie <144009897+OniWithTheHoodie@users.noreply.github.com>
2024-09-28 04:02:50 +0200

    component and link
    - made a component for the post-it
    - created a new route called sprint 13
    - conected sprint 13 and the component together so that when the component(post-it) it will go to the sprint 13 page

---
+++ src/lib/components/post-it/post-it.svelte
@@ -0,0 +1,36 @@
+<article class="post">
+    <a href="/sprint13" class="post__link">
+        <h2>sprint 13</h2>
+    </a>
+</article>
+
+<style>
+    .post {
+        display: flex;
+        justify-content: center;
+        width: 300px;
+        height: 300px;
+        border: 2px solid black;
+        box-shadow: black 10px 10px;
+        position: relative;
+    }
+
+    .post__link:before {
+        content: '';
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        background-color: transparent;
+    }
+
+    a {
+        text-decoration: none;
+    }
+
+    h2 {
+        color: black;
+        position: relative;
+    }
+</style>
